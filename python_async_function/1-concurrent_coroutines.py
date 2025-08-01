c4210ee285ba4df736521bf563381379d439d99d
kamisos3 <kamilasostre11@gmail.com>
2025-08-01 11:21:58 -0400

    Fixing pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """Prints delayed floats in ascending order"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    
+
     result = []
     for delay in delays:
         inserted = False
@@ -22,5 +22,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
                 break
         if not inserted:
             result.append(delay)
-    
+
     return result
