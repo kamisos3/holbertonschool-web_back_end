caba592134b3800caa02e9487d125610970c4a42
kamisos3 <kamilasostre11@gmail.com>
2025-08-01 10:17:17 -0400

    Converts float to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function that converts a float to string"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float"""
+    return str(n)
