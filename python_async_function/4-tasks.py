10c78be6ef5b37c6886fd484b45c391a055daaed
kamisos3 <kamilasostre11@gmail.com>
2025-08-01 11:46:33 -0400

    Handles multiple tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module that contains task_wait_n async routine"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """It handles multiple tasks, spawns task_wait_random n times with the specified max_delay"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    
+    result = []
+    for delay in delays:
+        inserted = False
+        for i in range(len(result)):
+            if delay < result[i]:
+                result.insert(i, delay)
+                inserted = True
+                break
+        if not inserted:
+            result.append(delay)
+    
+    return result
