f7137e97469be7484dce068e812822ffec12eb42
kamisos3 <kamilasostre11@gmail.com>
2025-08-01 10:11:57 -0400

    Returns floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function that returns the floor of a float"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float as an integer that is the first number making it a integer"""
+    return math.floor(n)
