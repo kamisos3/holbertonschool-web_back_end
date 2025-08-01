fe75a8368e46f2121e3accd420156a0c40ef12b5
kamisos3 <kamilasostre11@gmail.com>
2025-08-01 12:06:35 -0400

    fixingf pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,10 +8,13 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """It handles multiple tasks, spawns task_wait_random n times with the specified max_delay"""
+    """
+    It handles multiple tasks, spawns task_wait_random 
+    n times with the specified max_delay
+    """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    
+
     result = []
     for delay in delays:
         inserted = False
@@ -22,5 +25,5 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
                 break
         if not inserted:
             result.append(delay)
-    
+
     return result
