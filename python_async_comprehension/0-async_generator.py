fcb9fb0d2ea818f908541a75f7abc979270aee2a
kamisos3 <kamilasostre11@gmail.com>
2025-08-03 13:31:25 -0400

    Adding annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Async Generator module
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Loops 10 times, printing a float every second"""
     for _ in range(10):
         await asyncio.sleep(1)
