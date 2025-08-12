3d753c77b99707c03f5b1c0faeeffdfc651d47c7
kamisos3 <kamilasostre11@gmail.com>
2025-08-12 18:55:51 -0400

    Fixing indentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,5 @@
 def index_range(page, page_size):
     """Returns a tuple of size two that contains starting index"""
-
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
