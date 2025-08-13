19c6dec3cd244a8bad6fbd544e4505535c9d603d
kamisos3 <kamilasostre11@gmail.com>
2025-08-13 12:03:13 -0400

    Fixing pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,15 +2,16 @@
 from typing import Tuple
 """Imports tuple"""
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+
     """
     Return the start offset and end index for a given page and page size
 
-    page: This is the page number
-    page_size: Is the number of items per page
+    page: This is the page number thats 3
+    page_size: Is the number of items per page thats 15
 
     """
     start = (page - 1) * page_size
     end = start + page_size
-
     return (start, end)
