78fa468105c2b747ef76047a5e4b8549ed59d114
kamisos3 <kamilasostre11@gmail.com>
2025-08-13 11:59:08 -0400

    Fixing output

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,16 @@
+#!/usr/bin/python3
 from typing import Tuple
-
+"""Imports tuple"""
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return the start offset and end index for a given page and page size"""
+    """
+    Return the start offset and end index for a given page and page size
+
+    page: This is the page number
+    page_size: Is the number of items per page
+
+    """
     start = (page - 1) * page_size
     end = start + page_size
+
     return (start, end)
