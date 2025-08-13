291f3c09533ae765f437806550a9a45b58db4305
kamisos3 <kamilasostre11@gmail.com>
2025-08-12 21:30:54 -0400

    Adding notation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 from typing import Tuple
 
-def index_range(page, page_size):
-    """Returns a tuple of size two that contains starting index"""
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return the start and end index for a given page and page size"""
     start = (page - 1) * page_size
     end = page * page_size
 
