fb928d5cdc5bab99171e37f18eefb29b61b7a149
kamisos3 <kamilasostre11@gmail.com>
2025-08-12 22:00:55 -0400

    Function that returns tuple with starting index

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,6 +4,5 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return the start and end index for a given page and page size"""
     start = (page - 1) * page_size
-    end = page * page_size
-
+    end = start + page_size
     return (start, end)
