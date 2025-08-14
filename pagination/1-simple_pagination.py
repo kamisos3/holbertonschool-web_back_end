7d0259a6563f98220744665028fc1c29efeb37b8
kamisos3 <kamilasostre11@gmail.com>
2025-08-14 16:31:35 -0400

    Prints a specified row of a dataset

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+index_range = __import__('0-simple_helper_function').index_range
+
+res = index_range(1, 7)
+print(type(res))
+print(res)
+
+res = index_range(page=3, page_size=15)
+print(type(res))
+print(res)
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+           """Returning a specific row of a detaset"""
+           assert isinstance(page, int) and page > 0
+           assert isinstance(page_size, int) and page_size > 0
+
+           start, end = index_range(page, page_size)
+           data = self.dataset()
+
+           if start >= len(data):
+                return []
+
+           return data[start:end]
+ 
