9b61690281ef7ade6f6dcbff02134b4479f7c442
kamisos3 <kamilasostre11@gmail.com>
2025-08-17 19:03:43 -0400

    Inserts a new document based on key argument

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Inserts a new document to a collection"""
+
+def insert_school(mongo_collection, **kwargs):
+    """Using kwargs to insert a nee document to a collection"""
+    result = mongo_collection.kwargs(_id)
+
+    return result._id
