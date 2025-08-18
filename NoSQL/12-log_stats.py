8f1c79a081c88f38f91a3151e904f09155b3cd04
kamisos3 <kamilasostre11@gmail.com>
2025-08-17 20:45:33 -0400

    Fixing typo

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = collection.count_documents({"method":"GET", "path": "/status"})
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
