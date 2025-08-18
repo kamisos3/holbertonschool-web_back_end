81a89d662317d723e0d83e7aad85d76d67dad297
kamisos3 <kamilasostre11@gmail.com>
2025-08-17 20:48:55 -0400

    Fixing typo

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,18 +3,20 @@
 
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+def log_stats_report():
 
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    if __name__ == "__main__":
+        client = MongoClient('mongodb://127.0.0.1:27017')
+        collection = client.logs.nginx
 
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        total_logs = collection.count_documents({})
+        print(f"{total_logs} logs")
 
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+        print("Methods:")
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        for method in methods:
+            count = collection.count_documents({"method": method})
+            print(f"\tmethod {method}: {count}")
+
+        status_check = collection.count_documents({"method": "GET", "path": "/status"})
+        print(f"{status_check} status check")
