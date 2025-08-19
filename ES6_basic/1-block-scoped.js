4543c3ec4334f5f606ba1fd6f95419643a53e5fc
kamisos3 <kamilasostre11@gmail.com>
2025-08-19 19:20:07 -0400

    Fixing variable overwrite

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
 // Fixing variable overwrite in the inner block
-  let task = false;
-  let task2 = false;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
+    void task;
+    void task2;
   }
 
   return [task, task2];
