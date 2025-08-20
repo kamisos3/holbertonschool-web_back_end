a3cc0cc0c7b937d5bdfb035065ba61cd5c0d84a0
kamisos3 <kamilasostre11@gmail.com>
2025-08-20 15:28:20 -0400

    Fixing arrow function

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default function getNeighborhoodsList() {
+  // 
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = function add(newNeighborhood) {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
