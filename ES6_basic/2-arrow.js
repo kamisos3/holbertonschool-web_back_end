579dcc847f348f6cf8f91225bf7ac6795192fae4
kamisos3 <kamilasostre11@gmail.com>
2025-08-20 21:47:32 -0400

    Fixing arrow function

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -2,7 +2,7 @@ export default function getNeighborhoodsList() {
   // Rewritting to use arrow syntax
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  this.addNeighborhood = function add(newNeighborhood) {
+  this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
   };
