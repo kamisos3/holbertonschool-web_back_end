2dbf0d30cebfe2deba1093db3fd4c412b5781f34
kamisos3 <kamilasostre11@gmail.com>
2025-08-21 11:10:58 -0400

    Condensing code to print

---
+++ ES6_basic/3-default-parameter.js
@@ -0,0 +1,5 @@
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  // This condenses a function block (simplifies)
+
+  return initialNumber + expansion1989 + expansion2019;
+}
