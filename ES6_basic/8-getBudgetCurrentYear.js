f1173abe1aad506c9f735d15be313ab402d2f270
kamisos3 <kamilasostre11@gmail.com>
2025-08-21 23:58:01 -0400

    Prints budget of current year

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,11 +1,12 @@
 function getCurrentYear() {
-  return 2021;
+  const date = new Date();
+  return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   return {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita, 
-    };
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 }
