af9314587c7172ca478afd1ac7c700ef277aa125
kamisos3 <kamilasostre11@gmail.com>
2025-08-24 20:49:48 -0400

    Prints an evacutation warning message

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -14,6 +14,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
