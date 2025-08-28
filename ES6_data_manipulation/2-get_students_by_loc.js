7d91526844f1990d2ee82f34b82e143998fa32bc
kamisos3 <kamilasostre11@gmail.com>
2025-08-28 17:49:38 -0400

    Outputs only students from San Francisco arrays with filter()

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getStudentsByLocation(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.filter((student) => student.location == "San Francisco");
+}
