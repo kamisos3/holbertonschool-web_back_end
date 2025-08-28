b85e26d0ca8478a7139d17ac000b8775457a6f7e
kamisos3 <kamilasostre11@gmail.com>
2025-08-28 17:56:45 -0400

    Fixing === symbol

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,5 +2,5 @@ export default function getStudentsByLocation(students) {
   if (!Array.isArray(students)) {
     return [];
   }
-  return students.filter((student) => student.location == 'San Francisco');
+  return students.filter((student) => student.location === 'San Francisco');
 }
