53fad6034e18a8f3e6ab1beb57380c199ccb5443
kamisos3 <kamilasostre11@gmail.com>
2025-08-31 11:54:36 -0400

    Returns students grade

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,10 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const gradeObj = newGrades.map((g) => g.studentId === student.citys);
+      const gradeObj = newGrades.find((g) => g.studentId === student.id);
       return {
         ...student,
-        grade: gradeObj ? gradeObj.newGrades : 'N/A',
+        grade: gradeObj ? gradeObj.grade : "N/A",
       };
     });
-    console.log(student);
-    console.log(gradeObj);
 }
