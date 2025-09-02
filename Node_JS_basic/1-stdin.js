822f00ba38de6551fe9347d1db0c1b1124743331
kamisos3 <kamilasostre11@gmail.com>
2025-09-01 20:38:22 -0400

    Updating input method to print name

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (input) => {
-  console.log(`Your name is: ${input.trim()}`);
+  console.log('Your name is:' + input);
 });
 
 process.stdin.on('end', () => {
