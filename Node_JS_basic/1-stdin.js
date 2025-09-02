88f581b3c897048d53743afd5db2f004e00bde90
kamisos3 <kamilasostre11@gmail.com>
2025-09-01 20:33:56 -0400

    It welcomes student to input name and closes software

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (input) => {
+  console.log(`Your name is: ${input.trim()}`);
+  process.stdin.emit('end');
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
