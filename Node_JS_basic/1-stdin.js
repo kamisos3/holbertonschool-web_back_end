c1cfc01200b07081f17ea808fef294cfc64cc8c5
kamisos3 <kamilasostre11@gmail.com>
2025-09-03 19:13:08 -0400

    Fixing programs closure of task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding('utf8');
 
 let buffer = '';
 
-process.stdin.on('data', chunk => {
+process.stdin.on('data', (chunk) => {
   buffer += chunk;
 });
 
 process.stdin.on('end', () => {
   const name = buffer.trim();
-  
+
   console.log(`Your name is: ${name.trim()}`);
   console.log('This important software is now closing');
   process.exit(0);
