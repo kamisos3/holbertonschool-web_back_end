e989b33301695c057c32e5a426dba2c6d632f209
kamisos3 <kamilasostre11@gmail.com>
2025-09-05 12:47:02 -0400

    Updating file

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -5,8 +5,15 @@ const port = 1245;
 const path ='/any_endpoint';
 
 const app = createServer((req, res) => {
-    res.statusCode = 200;
-    res.end('Hello Holberton School!');
+    if (req.url === '/') {
+      res.statusCode = 200;
+      res.end('Hello Holberton School!');
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
 });
 
 app.listen(port, hostname, path);
+
+module.exports = app;
