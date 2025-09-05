751bd2f799ed32188d95c7953901b87325ca4d5a
kamisos3 <kamilasostre11@gmail.com>
2025-09-05 12:05:04 -0400

    Making a localhost http

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const { createServer } = require('node:http');
+
+const hostname = 'localhost';
+const port = 1245;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+});
+
+server.listen(port, hostname);
