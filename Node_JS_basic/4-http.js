a447af77ff97364e6e2ec9767af7c1501d66bba7
kamisos3 <kamilasostre11@gmail.com>
2025-09-05 12:18:25 -0400

    Updating file

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,10 +2,11 @@ const { createServer } = require('node:http');
 
 const hostname = 'localhost';
 const port = 1245;
+const path ='/any_endpoint';
 
 const server = createServer((app, res) => {
     res.statusCode = 200;
     res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname);
+server.listen(port, hostname, path);
