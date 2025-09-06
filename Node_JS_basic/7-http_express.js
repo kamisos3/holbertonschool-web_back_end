adc119be764782e6d420b3cd93788e65bc2c8f08
kamisos3 <kamilasostre11@gmail.com>
2025-09-06 19:39:28 -0400

    Now it can also load error

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -9,7 +9,7 @@ function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        reject(new Error('Cannot load the database\n'));
         return;
       }
       const lines = data.split('\n').filter((line) => line.trim() !== '');
