a30457c8b52432953bfefad74b30d650318b87a0
kamisos3 <kamilasostre11@gmail.com>
2025-09-06 19:20:13 -0400

    Similar to task 5, but uses express get and send modules

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 1245;
+const dbFile = process.argv[2];
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+      const byField = {};
+      students.forEach((row) => {
+        const cols = row.split(',').map((s) => s.trim());
+        const firstName = cols[0];
+        const field = cols[cols.length - 1];
+        if (!byField[field]) byField[field] = [];
+        byField[field].push(firstName);
+      });
+      resolve({
+        total: students.length,
+        byField,
+      });
+    });
+  });
+}
+
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.type('text/plain');
+  res.write('This is the list of our students\n');
+  try {
+    const stats = await countStudents(dbFile);
+    res.write(`Number of students: ${stats.total}\n`);
+    Object.entries(stats.byField).forEach(([field, list]) => {
+      res.write(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}\n`);
+    });
+    res.end();
+  } catch (err) {
+    res.status(500).send('Cannot load the database');
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
