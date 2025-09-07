7d258fb5ccacab2cb6f39d978129a298a07c7e55
kamisos3 <kamilasostre11@gmail.com>
2025-09-07 15:53:21 -0400

    Delinting

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,4 +1,4 @@
-import { readDatabase } from '../utils.js';
+import { readDatabase } from '../utils';
 
 export default class StudentsController {
   static async getAllStudents(_req, res) {
@@ -9,10 +9,10 @@ export default class StudentsController {
         a.toLowerCase().localeCompare(b.toLowerCase())
       );
       let message = 'This is the list of our students';
-      for (const field of fields) {
-        const list = data[field].join(", ");
-        message += `\nNumber of students in ${field}: ${data[field].length}. List: ${list}`;
-      }
+        fields.forEach((field) => {
+          const list = data[field].join(', ');
+          message += `\nNumber of students in ${field}: ${data[field].length}. List: ${list},`;
+        });
       res.status(200).send(message);
     } catch (err) {
       res.status(500).send('Cannot load the database');
